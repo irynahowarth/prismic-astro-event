a4edb203080d80c2d81f9faac7f6aecd7926e0b4
Iryna Howarth <irynahowarth@gmail.com>
2024-03-13 13:56:19 +0000

    [chore]: add setup file to connect Prismic to Astro

---
+++ src/prismic.js
@@ -0,0 +1,11 @@
+import Prismic from "@prismicio/client";
+
+const API_ENDPOINT = process.env.ASTRO_PUBLIC_PRISMIC_URL;
+const Client = Prismic.createClient(API_ENDPOINT);
+
+export function getAllCategories(tag) {
+  return Client.getByTag(tag);
+}
+export function getAllEvents() {
+  return Client.getAllByType("event");
+}
